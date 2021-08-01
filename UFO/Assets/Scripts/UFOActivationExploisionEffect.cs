e742361f2d260594d8c1d9a46ae6d0a20d45f184
KrSergei <34679066+KrSergei@users.noreply.github.com>
2021-08-01 22:49:12 +0300

    Added Exploision

--- UFO/Assets/Scripts/UFOActivationExploisionEffect.cs
+++ UFO/Assets/Scripts/UFOActivationExploisionEffect.cs
@@ -4,8 +4,19 @@ public class UFOActivationExploisionEffect : MonoBehaviour
 {
     public GameObject exploisionEffect;
 
+    public GameObject[] smokeEfect;
+
     public void ActivationExploision()
     {
         exploisionEffect.SetActive(true);
+        ActivationSmokeEffect();
+    }
+
+    public void ActivationSmokeEffect()
+    {
+        for (int i = 0; i < smokeEfect.Length; i++)
+        {
+            smokeEfect[i].SetActive(true);
+        }
     }
 }
